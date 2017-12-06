487f991dea49e4eff74cb352fd9f52f0e1e79c4d
Administrator <Administrator@USER-MOKGVEED8G>
2017-12-06 17:52:19 +0800

    Spring Boot & Spring HATEOAS

---
+++ springboot/src/main/java/com/showshine/study/SampleController.java
@@ -0,0 +1,29 @@
+package com.showshine.study;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@EnableAutoConfiguration
+public class SampleController extends SpringBootServletInitializer implements EmbeddedServletContainerCustomizer {
+
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(SampleController.class, args);
+    }
+
+	@Override
+	public void customize(ConfigurableEmbeddedServletContainer container) {
+		container.setPort(8081);  
+	}
+}
